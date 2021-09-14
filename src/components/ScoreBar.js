e51a456fe020d13d52bfd85eab3f4200b66eb530
Jonathan-zihindula <jonathanzihindula95@gmail.com>
2021-09-14 18:11:02 +0200

    showDropLevelList proprety setted

--- src/components/ScoreBar.js
+++ src/components/ScoreBar.js
@@ -9,14 +9,14 @@ const ScoreBar = (props) => {
             </div>
             <div className='score-div'>Score {props.score} </div>
             <div className="level-container">
-                <p className='level-para'>Level</p>
-                <div className="level-list">
+                <p className='level-para'>Level: { props.level}</p>
+                { props.showDropLevelList && <div className="level-list">
                     <ul className='level-list-ul'>
                         <li className='level-list-li' onClick={props.setLevel}>Low</li>
                         <li className='level-list-li' onClick={props.setLevel}>Medium</li>
                         <li className='level-list-li' onClick={props.setLevel}>Hight</li>
                     </ul>
-                </div>
+                </div>}
             </div>
             <div className='progress-bar'>
                 <progress className='progress-bar-dynamic' value={props.progress} max="100"></progress>
