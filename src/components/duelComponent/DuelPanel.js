2aba9222b18171e2435d3f685fdf1c8918270221
Jonathan-zihindula <jonathanzihindula95@gmail.com>
2021-09-26 05:27:32 +0200

    duel Panel

---
+++ src/components/duelComponent/DuelPanel.js
@@ -0,0 +1,23 @@
+import './DuelPanel.css';
+const DuelPanel = (props) => {
+    return (
+        <div className='duelPanel'>
+            <div className='duelPannel-button-controler'>
+                <button className='stop-duel'>Stop the duel</button>
+                <button className='show-duel-details'>Details</button>
+            </div>
+            <p className='duelLevel-container'>{props.duelLevel}</p>
+            <div className='duelCreator-panel'>
+                <p className='duelCreator-pseudo'>{props.duelCreatorPseudo}</p>
+                <p className='duelCreator-score'>{props.duelCreatorScore}</p>
+            </div>
+            <p className='versus-container'>VS</p>
+            <div className='joiner-pannel'>
+                <p className='joiner-pseudo'>{props.duelJoinerPseudo}</p>
+                <p className='joiner-score'>{props.duelJoinerScore}</p>
+            </div>
+        </div>
+    );
+}
+
+export default DuelPanel
