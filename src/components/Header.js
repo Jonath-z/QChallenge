e71ef12a3a8ca3720fe36ad90055406afba76b40
Jonathan-zihindula <jonathanzihindula95@gmail.com>
2021-10-08 20:05:21 +0200

    responsive to mobile phone is done

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,23 +1,17 @@
-// import { FaSearch } from 'react-icons/fa';
+import MediaQuery from 'react-responsive';
 import { useState,useEffect } from 'react';
 import { useHistory } from 'react-router';
-// import CryptoJS from 'crypto-js';
 import './Header.css';
+import { FiMenu } from 'react-icons/fi';
+import { MdCancel } from 'react-icons/md';
+
 
 
 const Header = (props) => {
     const [placeholder, setPlaceholder] = useState('Search');
-        // const [allUsers, setAllUser] = useState();
-    // const [searchPseudo, setSearchPseudo] = useState(null);
+    const [searchBarIsHiden, setSearchBarIsHidden] = useState(true);
+    const [showMenuSlide, setShowMenuSlide] = useState(false);
 
-    // useEffect(() => {
-    //     setAllUser(
-    //         () => {
-    //             const usersDecrypted = CryptoJS.AES.decrypt(localStorage.getItem('allUsers'), 'QChallenge001').toString(CryptoJS.enc.Utf8);
-    //             return JSON.parse(usersDecrypted);
-    //         }
-    //     )
-    // }, []);
     useEffect(() => {
         if (!props.searchClosed) {
             setFucusOut();
@@ -47,27 +41,66 @@ const Header = (props) => {
     const research = (e) => {
         props.setSearchInputValue(e.target.value);
     }
+   
     return (
         <div className="headerContainer">
-            <ul className='headerComponent'>
-                <li className='header-li'><h1 className='appNameNav'>QChallenge</h1></li>
-                <li className='header-li'><input
-                    type='search'
-                    className="searchBar"
-                    placeholder={placeholder}
-                    onFocus={setFucus}
-                    onBlur={setFucusOut}
-                    onChange={research}>
-                </input></li>
-                <li className='optionHeader option header-li'>Options
-                    <div className='Options-dropList'>
-                        <span className='duel-option' onClick={props.openDuel}>Create the duel</span><br/><br/>
-                        <span className='duel-option' onClick={props.openDuel}>Join the duel</span>
-                    </div>
-                </li>
-                <li className='documentationHeader option header-li'>Documentation</li>
-                <li className='AccountHeader option header-li' onClick={openAccount}>Account</li>
-            </ul>
+            <MediaQuery minWidth={300} maxWidth={414}>
+                <div className='header-responsive'>
+                    <ul className='headerComponent'>
+                        <li className='header-li'><h1 className='appNameNav'>QChallenge</h1></li>
+                        <li className='header-li'><input
+                            hidden={searchBarIsHiden}
+                            type='search'
+                            className="searchBar"
+                            placeholder={placeholder}
+                            onFocus={setFucus}
+                            onBlur={setFucusOut}
+                            onChange={research}
+                        /></li>
+                        <li className='header-li fiMenu'><FiMenu onClick={() => {
+                            if (showMenuSlide === false) {
+                                setShowMenuSlide(true);
+                            } else {
+                                setShowMenuSlide(false);
+                            }
+                        } }/></li>
+                    </ul>
+                    {showMenuSlide && <div className='header-slide-menue'>
+                        <ul className='ul-responsive'>
+                            <li className='optionHeader option header-li'>Options
+                                <div className='Options-dropList'>
+                                    <p className='duel-option' onClick={props.openDuel}>Create the duel</p><br /><br />
+                                    <p className='duel-option' onClick={props.openDuel}>Join the duel</p>
+                                </div>
+                            </li>
+                            <li className='documentationHeader option header-li'>Documentation</li>
+                            <li className='AccountHeader option header-li' onClick={openAccount}>Account</li>
+                        </ul>
+                    </div>}
+                </div>
+            </MediaQuery>
+            <MediaQuery minWidth={416} maxWidth={2000}>
+                <ul className='headerComponent'>
+                    <li className='header-li'><h1 className='appNameNav'>QChallenge</h1></li>
+                    <li className='header-li'><input
+                        type='search'
+                        className="searchBar"
+                        placeholder={placeholder}
+                        onFocus={setFucus}
+                        onBlur={setFucusOut}
+                        onChange={research}
+                    /></li>
+                    <li className='optionHeader option header-li'>Options
+                        <div className='Options-dropList'>
+                            <span className='duel-option' onClick={props.openDuel}>Create the duel</span><br /><br />
+                            <span className='duel-option' onClick={props.openDuel}>Join the duel</span>
+                        </div>
+                    </li>
+                
+                    <li className='documentationHeader option header-li'>Documentation</li>
+                    <li className='AccountHeader option header-li' onClick={openAccount}>Account</li>
+                </ul>
+            </MediaQuery>
         </div>
     );
 }
--- src/components/ScoreBar.js
+++ src/components/ScoreBar.js
@@ -1,9 +1,34 @@
 import './ScoreBar.css';
-import {FiCheck,FiX} from 'react-icons/fi'
+import { FiCheck, FiX } from 'react-icons/fi';
+import MediaQuery from 'react-responsive';
 
 const ScoreBar = (props) => {
     return (
         <div className='ScoreBar'>
+            <MediaQuery minWidth={300} maxWidth={414}>
+            <div className='chronoContainer'>
+                {props.chrono}S
+            </div>
+            <div className='score-div'>Score {props.score} </div>
+            <div className="level-container">
+                <p className='level-para'>Level: { props.level}</p>
+                { props.showDropLevelList && <div className="level-list">
+                    <ul className='level-list-ul'>
+                        <li className='level-list-li' onClick={props.setLevel}>Low</li>
+                        <li className='level-list-li' onClick={props.setLevel}>Medium</li>
+                        <li className='level-list-li' onClick={props.setLevel}>Hight</li>
+                    </ul>
+                </div>}
+                {props.showDropDuelLevelList &&
+                    <div className="level-list">
+                        <ul className='level-list-ul'>
+                        <li className='level-list-li' onClick={props.setLevel}>Set Duel level</li>
+                        </ul>
+                        </div>
+                }
+            </div>
+            </MediaQuery>
+            <MediaQuery minWidth={416} maxWidth={2000}>
             <div className='chronoContainer'>
                 {props.chrono}S
             </div>
@@ -31,6 +56,7 @@ const ScoreBar = (props) => {
                 {props.success ?<FiCheck className='chechAnswerSuccess' />:<FiX className='chechAnswerNoSuccess'/>}
 
             </div>
+            </MediaQuery>
         </div>
     );
 }
